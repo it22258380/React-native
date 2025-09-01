edb5f677c2045fd21a9e83923f00503d357ac0a5
Akash <it22258380@my.sliit.lk>
2025-09-01 16:56:45 +0530

    fix errors in category page

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { View } from "react-native";
+//import { Image,ScrollView,TextInput } from "react-native";
+
+import Category from "@/pages/Category";
+import React from "react";
+
+
+
+export default function Index() {
+  return (
+    <View>
+      <Category />
+      
+    </View>
+  );
+}
